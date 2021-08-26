8f2d871d9b0df1c732843ca092c230ce7594be58
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-26 14:32:05 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan ListView'),
+        ),
+        body: Column(
+          children: [
+            Text('Data ke 1'),
+            Text('Data ke 2'),
+          ],
+        ),
+      ),
+    );
+  }
+}
